d4d0b45e7cb903fc89c90a22f4cbb818273493a1
DarkVoid01 <arnoldeny15@gmail.com>
2022-09-28 17:18:36 +0100

    box grid ui

--- memory-game/src/components/BoxGrid/BoxGrid.jsx
+++ memory-game/src/components/BoxGrid/BoxGrid.jsx
@@ -3,10 +3,21 @@ import boxes from "./Boxes"
 import React from 'react'
 
 const BoxGrid = () => {
+
+    const clickHandler = e =>{
+        const number = e.target.child.textContent
+    }
   return (
     <div className="BoxGrid">
         <div className="box-container">
-            {boxes.map}
+            {boxes.map(
+                box =>(
+                    
+                <div onClick={clickHandler} className="indi-box" key={box.box}>
+                    <p>{box.value}</p>
+                </div>
+                )
+            )}
         </div>
     </div>
   )
--- memory-game/src/components/BoxGrid/Boxes.js
+++ memory-game/src/components/BoxGrid/Boxes.js
@@ -3,80 +3,96 @@ export default [
         box: '1',
         row: '1',
         column: '1',
+        value: '3'
     },
     {
         box: '2',
         row: '2',
         column: '1',
+        value: '4'
     },
     {
         box: '3',
         row: '3',
         column: '1',
+        value: '1'
     },
     {
         box: '4',
         row: '4',
         column: '1',
+        value: '8'
     },
     {
         box: '5',
         row: '1',
         column: '2',
+        value: '5'
     },
     {
         box: '6',
         row: '2',
         column: '2',
+        value: '7'
     },
     {
         box: '7',
         row: '3',
         column: '2',
+        value: '7'
     },
     {
         box: '8',
         row: '4',
         column: '2',
+        value: '5'
     },
     {
         box: '9',
         row: '1',
         column: '3',
+        value: '2'
     },
     {
         box: '10',
         row: '2',
         column: '3',
+        value: '6'
     },
     {
         box: '11',
         row: '3',
         column: '3',
+        value: '3'
     },
     {
         box: '12',
         row: '4',
         column: '3',
+        value: '8'
     },
     {
         box: '13',
         row: '1',
         column: '4',
+        value: '2'
     },
     {
         box: '14',
         row: '2',
         column: '4',
+        value: '1'
     },
     {
         box: '15',
         row: '3',
         column: '4',
+        value: '6'
     },
     {
         box: '16',
         row: '4',
         column: '4',
+        value: '4'
     },
 ]
