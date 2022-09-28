baadd7f91fb8bb3b8313947b2c3afc4508b27ac7
DarkVoid01 <arnoldeny15@gmail.com>
2022-09-28 15:45:31 +0100

    box grid

---
+++ memory-game/src/components/BoxGrid/BoxGrid.jsx
@@ -0,0 +1,15 @@
+import "./BoxGrid.css"
+import boxes from "./Boxes"
+import React from 'react'
+
+const BoxGrid = () => {
+  return (
+    <div className="BoxGrid">
+        <div className="box-container">
+            {boxes.map}
+        </div>
+    </div>
+  )
+}
+
+export default BoxGrid
---
+++ memory-game/src/components/BoxGrid/Boxes.js
@@ -0,0 +1,82 @@
+export default [
+    {
+        box: '1',
+        row: '1',
+        column: '1',
+    },
+    {
+        box: '2',
+        row: '2',
+        column: '1',
+    },
+    {
+        box: '3',
+        row: '3',
+        column: '1',
+    },
+    {
+        box: '4',
+        row: '4',
+        column: '1',
+    },
+    {
+        box: '5',
+        row: '1',
+        column: '2',
+    },
+    {
+        box: '6',
+        row: '2',
+        column: '2',
+    },
+    {
+        box: '7',
+        row: '3',
+        column: '2',
+    },
+    {
+        box: '8',
+        row: '4',
+        column: '2',
+    },
+    {
+        box: '9',
+        row: '1',
+        column: '3',
+    },
+    {
+        box: '10',
+        row: '2',
+        column: '3',
+    },
+    {
+        box: '11',
+        row: '3',
+        column: '3',
+    },
+    {
+        box: '12',
+        row: '4',
+        column: '3',
+    },
+    {
+        box: '13',
+        row: '1',
+        column: '4',
+    },
+    {
+        box: '14',
+        row: '2',
+        column: '4',
+    },
+    {
+        box: '15',
+        row: '3',
+        column: '4',
+    },
+    {
+        box: '16',
+        row: '4',
+        column: '4',
+    },
+]
